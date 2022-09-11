741030a492332a78053dce274768758a8c30bd38
Argemiro <argemiro.jesus@gmail.com>
2022-09-11 13:11:19 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
+import './style.css';
 
-import './style.css'
 function NotificationButton() {
     return (
         <div className="dsmeta-red-btn">
@@ -9,4 +9,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
