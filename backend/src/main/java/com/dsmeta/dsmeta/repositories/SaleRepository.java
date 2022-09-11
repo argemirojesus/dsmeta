890578441f900b103469c18aee8aa1e34500f136
Argemiro <argemiro.jesus@gmail.com>
2022-09-11 15:13:25 -0300

    API Test

---
+++ backend/src/main/java/com/dsmeta/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.dsmeta.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dsmeta.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+}
+
